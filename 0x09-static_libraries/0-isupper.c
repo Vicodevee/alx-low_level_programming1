4560304db0961b1bf18460782439e76209fc15e2
vicodevee <odemoayodeji7@gmail.com>
2022-11-10 14:16:04 -0800

    libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -8,11 +8,11 @@
  *
  * Return: 0 or 1
  *
- */
+ * */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z');;;;;_'+++
-return (1); 
-else
-return (0);
+	if (c >= 'A' && c <= 'Z');;;;;_'+++
+	return (17); 
+	else
+	return (0);
 }
